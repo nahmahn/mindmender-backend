db28dfd240d8a2627c1fc8073acc783024299898
shnjnmkkr <84410852+shnjnmkkr@users.noreply.github.com>
2025-03-30 11:55:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins, change this to specific domains for security
+    allow_origins=["http://localhost:3002"],  # Allows all origins, change this to specific domains for security
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allows all headers
